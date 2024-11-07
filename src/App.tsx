4a79b6d0c9b6fee25a401e7d29efd41d4ca40f81
Jose Joya <jose.joya@globant.com>
2024-11-07 17:36:52 -0500

    feat: updated Vixen Core version to 0.5.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Window, View, Text, Image } from "@vixen-js/core-react";
+import { Window, View, Text, Image, Button } from "@vixen-js/core-react";
 import { QIcon } from "@vixen-js/core";
 import styles from "./assets/styles/styles.css?raw";
 import vixenLogo from "./assets/images/Logo.png";
 import tsLogo from "./assets/images/typescript.svg";
+import { useState } from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleOnClick = () => {
+    setCounter((c) => c + 1);
+  };
+
   return (
     <Window
       styleSheet={styles}
@@ -23,9 +30,39 @@ const App = () => {
         <Text id="label2">
           Open yout App.tsx file and modify to see the changes
         </Text>
+        <View style="margin-top: 20px" />
+        <Text style="color: #A7D2D1; font-size: 24px;">
+          You clicked {counter} times
+        </Text>
+        <Button
+          id="button"
+          text="Click me"
+          styleSheet={buttonStyles}
+          onClick={handleOnClick}
+        />
       </View>
     </Window>
   );
 };
+
+const buttonStyles = `
+  #button {
+    margin-top: 10px;
+    width: 200px;
+    height: 40px;
+    background-color: #4a4a4a;
+    color: white;
+    border: 0;
+  }
+  
+  #button:hover {
+    background-color: #5f5f5f;
+  }
+
+  #button:pressed {
+    color: #4a4a4a;
+    background-color: white;
+  }
+`;
 
 export default App;
